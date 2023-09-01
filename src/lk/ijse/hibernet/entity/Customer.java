122dc00fe1dd4d38df4189180f97a34aca3a9b59
jayani018 <jayaniarunika11@gmail.com>
2023-09-02 01:55:05 +0530

    Added to Customer Entity

---
+++ src/lk/ijse/hibernet/entity/Customer.java
@@ -0,0 +1,65 @@
+package lk.ijse.hibernet.entity;
+
+/**
+ * @author : Jayani_Arunika  9/2/2023 - 1:51 AM
+ * @since : v0.01.0
+ **/
+
+public class Customer {
+    String id;
+    String name;
+    String address;
+    Double salary;
+
+    public Customer() {
+    }
+
+    public Customer(String id, String name, String address, Double salary) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.salary = salary;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
